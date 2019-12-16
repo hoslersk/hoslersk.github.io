2b56b60a21893a18b431112426fa0bf4fd2c4256
Schyler Hosler <schylerhosler@gmail.com>
2019-12-15 23:31:48 -0500

    Updates

---
+++ precache-manifest.01d636e299a2f4095106434e49c0a02e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f422dfb5cf1e6576dd41ca7a727699",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58a91dd45049f74e601b",
+    "url": "/static/css/main.5041189b.chunk.css"
+  },
+  {
+    "revision": "6091d22d585c71077132",
+    "url": "/static/js/2.68d95249.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.68d95249.chunk.js.LICENSE"
+  },
+  {
+    "revision": "58a91dd45049f74e601b",
+    "url": "/static/js/main.3f2768a6.chunk.js"
+  },
+  {
+    "revision": "eff7012e0748a65a5243",
+    "url": "/static/js/runtime-main.b36f495b.js"
+  }
+]);
