24f8472f03f8fd6ca316f1b4a8244b0c861af468
Schyler <schyler.hosler@vshift.com>
2019-12-16 10:48:44 -0500

    Updates

---
+++ precache-manifest.dcf1555c83829f7ad406d997ec306d08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e646b28ecb6e8c48686fdb60ec2c55b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7857a305ca5534aeae2",
+    "url": "/static/css/main.5041189b.chunk.css"
+  },
+  {
+    "revision": "6091d22d585c71077132",
+    "url": "/static/js/2.68d95249.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/static/js/2.68d95249.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b7857a305ca5534aeae2",
+    "url": "/static/js/main.157c4eb2.chunk.js"
+  },
+  {
+    "revision": "eff7012e0748a65a5243",
+    "url": "/static/js/runtime-main.b36f495b.js"
+  }
+]);
--- src/components/heading.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import './heading.scss';
-
-export default function Heading({ children }) {
-  return <h1 className="heading">{children}</h1>;
-}
